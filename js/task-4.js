0b1f11880a3ba5ab986e37dc5530a47d3f4e683d
Kosta-777 <150023312+Kosta-777@users.noreply.github.com>
2024-02-09 20:56:51 +0100

    1

--- js/task-4.js
+++ js/task-4.js
@@ -1,29 +1,17 @@
-function getShippingCost(country) {
 
-    let price = 0;
+function slugify(title) {
 
-switch (country) {
-        case "China" :  price = 100;
-        break;
-    case "Chile":  price = 250;
-        break;
-    case "Australia":  price = 170;
-        break;
-    case "Jamaica":  price = 120;
-        break;
-    default: return "Sorry, there is no delivery to your country";
- 
-    
-    }
-    
+  let titleSmallLetters = title.toLowerCase();
 
-    return `Shipping to ${country} will cost ${price} credits`;
+  let titleToArray = titleSmallLetters.split (" ");
 
+  let titleArrayToString = titleToArray.join("-");
+
+  return titleArrayToString;
 
 }
-console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
-console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
-console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
-console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
-console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
+
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
